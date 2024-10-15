33575a73544f458e5dd7a103b547321fa9146a49
Tran Thien An PHAN <phantranthienan1405@gmail.com>
2024-10-15 10:51:21 +0200

    solve Majority Element

---
+++ Array&String/MajorityElement.py
@@ -0,0 +1,15 @@
+# 169. Majority Element
+
+class Solution(object):
+    def majorityElement(self, nums):
+        major_element = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                major_element = num
+                count = 1
+            elif major_element == num:
+                count += 1
+            else:
+                count -= 1
+        return major_element
